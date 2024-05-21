02ccbf76ed534d779fe160b6562fd3cdba057f60
Kingsley Ijomah <git-example@codehance>
2024-05-21 15:32:02 -0300

    Adding appflow sdk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,17 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'photo-gallery',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    LiveUpdates: {
+      appId: '48fb1c8a',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
+  }
 };
 
 export default config;
